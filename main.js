7ca95ca897ed3bd915d08c8d2d9c2ec09444a6ac
Travotavo <tcarlen@live.com>
2023-04-11 15:56:22 -0700

    Tutorial Followed to end!

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const defaultAttributeScores = [15, 14, 13, 12, 10, 8];
+
 class Player {
     constructor(characterName = "Anon"){
         this.name = characterName;
@@ -9,17 +11,60 @@ class Player {
             wisdom: 0,
             charisma: 0
         }
+
+        let shuffledResult = shuffleArray(defaultAttributeScores);
+        for (const [key, value] of Object.entries(this.attributes)) {
+            let attributeValue = shuffledResult.pop();
+            this.attributes[key] = attributeValue;
+        }
     }
 
     rollAttributes(){
-        console.log("Rolling Attributes!");
+        for (const key in this.attributes) {
+            let results = diceRoller(4, 6);
+            results.sort(function(a, b){return a - b}); // numeric sort w/ compare function
+            results.shift(); // remove lowest die roll
+            let sum = sumArrayElements(results); // sum the rolls
+            this.attributes[key] = sum;
+        }
     }
     
     printCharacter(){
-        console.log(this.name);
-        console.log(this.attributes);
+        console.log(`NAME: ${this.name}`);
+        for (let [key, i] of Object.entries(this.attributes)){
+            console.log(`${key.slice(0,3).toUpperCase()}: ${i}`);
+        }
     }
+
+    
 }
+
+function diceRoller(times, sides) {
+    let results = [];
+    for (let i = 0; i < times; i++) {
+        results.push(Math.floor(Math.random() * sides + 1));
+    }
+
+    return results;
+}
+
+function sumArrayElements(array) {
+    return array.reduce((total, currentNumber) => total + currentNumber);
+}
+// Fisher-Yates algorithm for randomly sorting an array
+// from: https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
+// adapted to JS and reconfigured to return a new (non-mutated) array
+function shuffleArray(targetArray) {
+    let shuffled = Array.from(targetArray);
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = shuffled[i];
+        shuffled[i] = shuffled[j];
+        shuffled[j] = temp;
+    }
+    return shuffled;
+}
+
 
 const Player1  = new Player();
 const Player2 = new Player("Beegee");
